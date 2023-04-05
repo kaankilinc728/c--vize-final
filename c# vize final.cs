5372405f99181f2167c5cf34b47685d73dc08241
kaan <kaankilinc22@gmail.com>
2023-04-05 15:14:08 +0300

    vize final ort hesaplama

---
+++ c# vize final.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kullanıcıdan_notlarını_istiyoruz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int VizeNot;
+            Console.WriteLine("Vize notunuzu girin: ");
+            VizeNot = Convert.ToInt32(Console.ReadLine());
+
+            int FinalNot;
+            Console.WriteLine("Fİnal notunuzu girin: ");
+            FinalNot = Convert.ToInt32(Console.ReadLine());
+
+            double Ortalama;
+            Ortalama = (VizeNot * 0.4) + (FinalNot * 0.6);
+            Console.WriteLine("Orrtalamanız:" + Ortalama);
+            Console.ReadLine();
+
+
+        }
+    }
+}
